2e5f13c40e555121554ffb52917bf490e9914265
Per Enström <enstrom.per@gmail.com>
2017-03-04 15:28:15 +0100

    Added bodyparser to parse form data

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const settings = require('./settings.json');
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(settings.port, function() {
   console.log("Listening on port", settings.port);
 });
 
 app.get("/", function (req, res) {
-    res.sendfile("frontend/index.html");
+    res.sendFile(__dirname + "/frontend/index.html");
+});
+
+app.post("/quotes", function (req, res) {
+    console.log(req.body);
 });
