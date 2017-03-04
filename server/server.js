6c6ee5b71b3123af1188a820215be87b0e9c8687
Per Enström <enstrom.per@gmail.com>
2017-03-04 14:56:36 +0100

    Added devstart what he said

--- server/server.js
+++ server/server.js
@@ -5,3 +5,7 @@ const app = express();
 app.listen(settings.port, function() {
   console.log("Listening on port", settings.port);
 });
+
+app.get("/", function (req, res) {
+    res.send("<h1>Hello world</h1><h2>Second line!</h2>");
+});
