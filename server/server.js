5ac6df809a15649e7743c362399a64ee33aa3591
Filip Tronnberg <filip@Filips-MacBook-Pro.local>
2017-03-04 15:43:58 +0100

    Added database

--- server/server.js
+++ server/server.js
@@ -2,12 +2,24 @@ const express = require('express');
 const settings = require('./settings.json');
 const bodyParser = require("body-parser");
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+let db;
+
+MongoClient.connect('mongodb://demouser:Alligator3@ds115870.mlab.com:15870/express-mongo-node-demo', (err, database) => {
+	if (err) {
+		console.log("Database error occured", err);
+		return;
+	}
+	
+	db = database;	
+	app.listen(settings.port, function() {
+  		console.log("Listening on port", settings.port);
+	});
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(settings.port, function() {
-  console.log("Listening on port", settings.port);
-});
+
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/frontend/index.html");
